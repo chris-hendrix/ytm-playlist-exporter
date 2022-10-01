1f4264eaa8917ff1afa53ddf7789ce0c8cf93be6
Chris Hendrix <chendrix1123@gmail.com>
2022-10-01 14:06:24 -0400

    initial setup

---
+++ app.py
@@ -0,0 +1,4 @@
+from ytmusicapi import YTMusic
+
+if __name__ == "__main__":
+    YTMusic.setup(filepath='headers_auth.json')
---
+++ setup.py
@@ -0,0 +1,3 @@
+# https://stackoverflow.com/a/50194143
+from setuptools import setup, find_packages
+setup(name='ytm-to-csv', version='1.0', packages=find_packages())
